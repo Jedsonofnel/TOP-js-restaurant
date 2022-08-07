9ba53c2c91efb927a8d4472f1f2df3bcd1116ec0
jedn <jed@nelson.ac>
2022-08-07 14:41:39 +0200

    Css module config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -26,6 +26,29 @@ module.exports = {
                 type: "asset/resource",
             },
             {
+                test: /\.css$/i,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            modules: true,
+                        },
+                    },
+                ],
+                include: /\.module\.css$/i,
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+                exclude: /\.module\.css$/i,
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+            {
                 test: /\.(png|jpe?g|webp|tiff)$/i,
                 use: [
                     {
